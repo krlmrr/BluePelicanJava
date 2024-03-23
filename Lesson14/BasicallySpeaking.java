917be4bdaa3fa558368f453fcbfe9d606186ed48
Karl Murray <karl@karlm.dev>
2024-03-23 01:02:56 -0500

    Finish BasicallySpeaking

---
+++ Lesson14/BasicallySpeaking.java
@@ -0,0 +1,23 @@
+package Lesson14;
+
+public class BasicallySpeaking {
+    public static void main(String[] args) {
+        System.out.print("Decimal  ");
+        System.out.print("Binary   ");
+        System.out.print("Octal  ");
+        System.out.print("Hex  ");
+        System.out.println("Character");
+
+        for(int i = 65; i <= 90; i++ ) {
+            System.out.print(i);
+            System.out.print("       ");
+            System.out.print(Integer.toBinaryString(i));
+            System.out.print("  ");
+            System.out.print(Integer.toOctalString(i));
+            System.out.print("    ");
+            System.out.print(Integer.toHexString(i));
+            System.out.print("   ");
+            System.out.println((char) i);
+        }
+    }
+}
