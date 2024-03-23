700defcd591a557ebc0e55b2c4491024ea0fc4c0
Karl Murray <karl@karlm.dev>
2024-03-23 01:03:57 -0500

    A little nicer formatting.

--- Lesson14/BasicallySpeaking.java
+++ Lesson14/BasicallySpeaking.java
@@ -3,7 +3,7 @@ package Lesson14;
 public class BasicallySpeaking {
     public static void main(String[] args) {
         System.out.print("Decimal  ");
-        System.out.print("Binary   ");
+        System.out.print("Binary    ");
         System.out.print("Octal  ");
         System.out.print("Hex  ");
         System.out.println("Character");
@@ -12,7 +12,7 @@ public class BasicallySpeaking {
             System.out.print(i);
             System.out.print("       ");
             System.out.print(Integer.toBinaryString(i));
-            System.out.print("  ");
+            System.out.print("   ");
             System.out.print(Integer.toOctalString(i));
             System.out.print("    ");
             System.out.print(Integer.toHexString(i));
